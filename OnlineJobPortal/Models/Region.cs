aa0058175b7c2a4603a0bcdc8c34b583b955b428
ZekariyasGirma <Zekariyas.Girma.XY8281>
2022-09-26 22:21:12 +0300

    Added Region Model

--- OnlineJobPortal/Models/Region.cs
+++ OnlineJobPortal/Models/Region.cs
@@ -1,6 +1,19 @@
-﻿namespace OnlineJobPortal.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+namespace OnlineJobPortal.Models
 {
     public class Region
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Region Name")]
+        public string RegionName { get; set; }
     }
 }
