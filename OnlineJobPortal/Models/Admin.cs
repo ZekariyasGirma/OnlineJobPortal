371a0f1b0717a81e38ae3c884612dc63ef470567
Zekariyas Girma <Zekariyas Girma@DESKTOP-A817T39>
2022-09-26 13:40:06 +0300

    Add project files.

---
+++ OnlineJobPortal/Models/Admin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace OnlineJobPortal.Models
+{
+    public class Admin
+    {
+        [Key]
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+       
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [DisplayName(displayName:"Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
